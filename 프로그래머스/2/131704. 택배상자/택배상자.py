f95e08b0464777bc5ce40ee004d08ea1dadbe2ca
KyuWon  Kim <80209277+kkyu0718@users.noreply.github.com>
2024-02-25 13:07:21 +0900

    [level 2] Title: 택배상자, Time: 323.73 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -4,22 +4,8 @@ def solution(order):
     idx = 0
     stk = []
     for i in range(1, n+1):
-        if i != order[idx]: 
-            while stk and order[idx] == stk[-1]:
-                answer.append(stk.pop())
-                idx += 1
-            # stk처리
-            if i != order[idx]:
-                stk.append(i)
-            else:
-                answer.append(i)
-                idx+= 1
-        else:
-            answer.append(i)
+        stk.append(i)
+        while stk and stk[-1] == order[idx]:
+            answer.append(stk.pop())
             idx += 1
-        # print(answer)
-    # stk 처리
-    while stk and order[idx] == stk[-1]:
-        answer.append(stk.pop())
-        idx += 1
     return len(answer)

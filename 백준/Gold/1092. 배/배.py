901d09187067f14b15bad76bbf9681dfc0245906
KyuWon  Kim <80209277+kkyu0718@users.noreply.github.com>
2024-03-09 16:16:22 +0900

    [Gold V] Title: 배, Time: 112 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.py
@@ -0,0 +1,23 @@
+n = int(input())
+cranes = list(map(int, input().split()))
+m = int(input())
+boxes = list(map(int, input().split()))
+
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
+
+cnt = 0
+
+if boxes[0] > cranes[0]: cnt = -1
+else:
+    while boxes:
+        for c in cranes:
+            if boxes and c < boxes[-1]:
+                continue
+            for b in boxes:
+                if c >= b:
+                    boxes.remove(b)
+                    break
+        cnt += 1
+        
+print(cnt)
